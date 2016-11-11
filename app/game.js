452aa9653a0dddb8dc2c7bc7764e36e967a94104
Erik Larsson <erik.mattias.larsson@gmail.com>
2016-11-11 19:41:54 +0100

    Player bullets and prevent defaults

--- app/game.js
+++ app/game.js
@@ -34,15 +34,58 @@ define('app/game', [
         }
     }
 
+    class PlayerBullet extends GameObject {
+        constructor(config) {
+            super(config)
+            this.speed = 4;
+            this.direction = config.direction;
+        }
+        tick() {
+            this.setVelocityXY(this.direction.x * this.speed,this.direction.y * this.speed)
+        }
+        draw() {
+            context.fillStyle = "red";
+            context.fillRect(this.position.x, this.position.y, this.width, this.height);
+        }
+    }
+
     class Player extends GameObject {
         constructor(config) {
             super(config)
+            this.speed = 3;
+            this.direction = { x: 0, y: 0 }
+            this.recharge = 0;
         }
         tick() {
-            this.setVelocityXY(1,0)
+            const pad = userInput.getInput(0)
+
+            var velocity = {
+                x: 0,
+                y: 0
+            }
+            velocity.x = velocity.x + (pad.axes[0] * this.speed);
+            velocity.y = velocity.y + (pad.axes[1] * this.speed);
+
+            if (!(velocity.x === 0 && velocity.y === 0))
+                this.direction = velocity;
+
+            this.setVelocityXY(velocity.x,velocity.y)
+
+            if (pad.buttons[2].pressed && this.recharge === 0) {
+                this.recharge = 10;
+                var playerBullet = new PlayerBullet({
+                    direction: _.clone(this.direction),
+                    width: 20,
+                    height: 20
+                });
+                playerBullet.setPositionXY(this.position.x + this.width/2 - 10, this.position.y + this.height/2 - 10);
+                playerBullet.setVelocityXY(0, 0)
+                gameObjects.push(playerBullet)
+            } else if (this.recharge > 0) {
+                this.recharge = this.recharge - 1;
+            }
         }
         draw() {
-            //super.draw()
             context.drawImage(images.player, this.position.x, this.position.y);
         }
     }
@@ -92,6 +135,12 @@ define('app/game', [
             Krocka.run({
                 objects: gameObjects,
                 detector: function (gameObject, other) {
+                  /*var filter = [[Player, Enemy]];
+                  var ignore = _.filter(filter, function(pair) {
+                     return (gameObject instanceof pair[0] && other instanceof pair[1] ||
+                        gameObject instanceof pair[1] && other instanceof pair[0])
+                  })
+                  if (ignore.length > 1) return false;*/
                   if (!other.markedForRemoval && !gameObject.markedForRemoval) {
                     return Krocka.detectAABBtoAABB(gameObject, other)
                   }
@@ -99,7 +148,8 @@ define('app/game', [
                 },
                 resolver: function (collision) {
 
-                  collision.resolveByType(Player, Enemy, function (player, enemy) {
+                  collision.resolveByType(PlayerBullet, Enemy, function (playerBullet, enemy) {
+                    playerBullet.destroy();
                     enemy.destroy();
                   })
 
